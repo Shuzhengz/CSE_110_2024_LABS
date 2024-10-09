edb7746f10c6cc23b7778ac891261856395450a7
Tom Zhang <treez.zhang@gmail.com>
2024-10-09 15:38:19 -0700

    adds title box

--- LAB2/my-app/src/App.tsx
+++ LAB2/my-app/src/App.tsx
@@ -8,6 +8,16 @@ function App() {
                     x
                 </button>
                 <h2> 1st Note Title </h2>
+                <form className="note-form">
+                    <div><input placeholder="Note Title"></input></div>
+
+                    <div><textarea></textarea></div>
+
+                    <div>
+                        <button type="submit">Create Note</button>
+                    </div>
+                </form>
+                
                 <p> 1st Note Content </p>
                 <p> 1st Note Label </p>
             </div>
