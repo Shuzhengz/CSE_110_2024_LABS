5012e931c49f103793a00308ebc2554871bca334
Tom Zhang <treez.zhang@gmail.com>
2024-10-10 22:44:38 -0700

    4.2.3 done

--- LAB2/my-app/src/hooksExercise.tsx
+++ LAB2/my-app/src/hooksExercise.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { ThemeContext, themes } from "./themeContext";
 
 export function ClickCounter() {
@@ -7,16 +7,30 @@ export function ClickCounter() {
     const handleClick = () => {
         setCount(count + 1);
     };
+    
     useEffect(() => {
         document.title = `You clicked ${count} times`;
     }, [count]);
 
+    const theme = useContext(ThemeContext);
     return (
-        <div>
-            <p>Clicks: {count}</p>
-            <button onClick={handleClick}>Click me!</button>
+        <div
+            style={{
+                background: theme.background,
+                color: theme.foreground,
+                padding: "20px",
+            }}
+        >
+            <p>You clicked {count} times </p>
+            <button
+                onClick={() => setCount(count + 1)}
+                style={{ background: theme.foreground, color: theme.background }}
+            >
+                Click me
+            </button>
         </div>
     );
+
 }
 
 // Wrapper component to provide context
