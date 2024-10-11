fecebf4e2c0c95a127b4c9ddb13697aaef61f497
Tom Zhang <treez.zhang@gmail.com>
2024-10-10 22:42:49 -0700

    init themeContext

--- LAB2/my-app/src/hooksExercise.tsx
+++ LAB2/my-app/src/hooksExercise.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { ThemeContext, themes } from "./themeContext";
 
 export function ClickCounter() {
     const [count, setCount] = useState(0);
@@ -6,7 +7,6 @@ export function ClickCounter() {
     const handleClick = () => {
         setCount(count + 1);
     };
-
     useEffect(() => {
         document.title = `You clicked ${count} times`;
     }, [count]);
@@ -18,3 +18,21 @@ export function ClickCounter() {
         </div>
     );
 }
+
+// Wrapper component to provide context
+function ToggleTheme() {
+    const [currentTheme, setCurrentTheme] = useState(themes.light);
+
+    const toggleTheme = () => {
+        setCurrentTheme(currentTheme === themes.light ? themes.dark : themes.light);
+    };
+
+    return (
+        <ThemeContext.Provider value={currentTheme}>
+            <button onClick={toggleTheme}> Toggle Theme </button>
+            <ClickCounter />
+        </ThemeContext.Provider>
+    );
+}
+
+export default ToggleTheme;
